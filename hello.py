64e33e5ef5da6c65e7eff081d98234a76f3191c3
mollechu <mollychung123@hotmail.com>
2019-12-02 09:36:21 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,18 +2,4 @@ from flask import Flask, render_template
 
 app=Flask("MyApp")
 
-@app.route("/")
-def hello():
-    return("something other than Hello World!")
-
-@app.route("/<name>")
-def hello_someone(name):
-    return render_template("hello.html", name=name.title())
-
-@app.route("/signup", methods=["POST"])
-def sign_up():
-    form_data = request.form
-    print(form_data["email"])
-    return("All OK")
-
 app.run(debug=True)
