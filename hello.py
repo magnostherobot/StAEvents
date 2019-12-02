11c7dc3b7c19b996b1eb2b7ac50f3360d381ebc1
Tom Harley <trh@st-andrews.ac.uk>
2019-12-02 17:03:19 +0000

    fixes

--- hello.py
+++ hello.py
@@ -1,5 +1,19 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 
-app=Flask("MyApp")
+app=Flask("MyApp", static_url_path="")
 
-app.run(debug=True)
+@app.route("/")
+def serve_index():
+    return render_template("index.html")
+
+@app.route("/assets/<path:filename>")
+def serve_assets(filename):
+    return send_from_directory("assets", filename)
+
+@app.route("/scripts/<path:filename>")
+def serve_scripts(filename):
+    return send_from_directory("scripts", filename)
+
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host="0.0.0.0", port=port)
